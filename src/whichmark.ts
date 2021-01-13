60aaa0d68c4a2afc465f25f3cc7bdd23ea2ed8b4
coalpha <22552895+coalpha@users.noreply.github.com>
2021-01-12 17:34:12 -0800

    It works but not fast.
    Had to go with the blocking version.

--- src/whichmark.ts
+++ src/whichmark.ts
@@ -6,7 +6,7 @@ type Nullmark = Bookmark | null;
 type Consumer<T> = (res: T) => void;
 type Runnable    = ()       => void;
 
-const childrenGetChildByTitle = (children: Bookmark[], title: string): Nullmark => {
+function childrenGetChildByTitle(children: Bookmark[], title: string): Nullmark {
    const len = children.length;
    for (var i = 0; i < len; ++i) {
       const child = children[i];
@@ -17,22 +17,22 @@ const childrenGetChildByTitle = (children: Bookmark[], title: string): Nullmark 
    return null;
 }
 
-const getChildByTitle = (parentId: string, title: string, cb: Consumer<Nullmark>) =>
-B.getChildren(parentId, children => cb(childrenGetChildByTitle(children, title)))
+function getChildByTitle(parentId: string, title: string, cb: Consumer<Nullmark>) {
+   B.getChildren(parentId, children =>
+      cb(childrenGetChildByTitle(children, title)));
+}
 
-const openFolder = (parentId: string, title: string, cb: Consumer<Bookmark>) =>
+function openFolder(parentId: string, title: string, cb: Consumer<Bookmark>) {
    B.getChildren(parentId, children => {
       const presentChild = childrenGetChildByTitle(children, title);
       if (presentChild) {
-         console.info(`${title} was present`);
          cb(presentChild);
          return;
       }
 
-      console.info(`${title} is being created`);
       B.create({title, parentId}, cb);
-   })
-
+   });
+}
 
 const root_id    = "root________";
 const toolbar_id = "toolbar_____";
@@ -41,15 +41,21 @@ const other_id   = "unfiled_____";
 const numberOfToolbars = 2;
 const benchedToolbarIds: string[] = Array(numberOfToolbars);
 
-console.info("starting");
 openFolder(other_id, "Switchmark", registerToolbars);
 
 function registerToolbars(whichmark: Bookmark) {
-   const loop = (i: number): void =>
-      i < numberOfToolbars
-      ? openFolder(whichmark.id, i.toString(), toolbar => {benchedToolbarIds[i] = toolbar.id; loop(i + 1)})
-      : registerActiveToolbarIndex()
-   loop(0);
+   openToolbar(0);
+   function openToolbar(i: number) {
+      if (i === numberOfToolbars) {
+         registerActiveToolbarIndex();
+         return;
+      }
+
+      openFolder(whichmark.id, i.toString(), toolbar => {
+         benchedToolbarIds[i] = toolbar.id;
+         openToolbar(i + 1);
+      });
+   }
 }
 
 var activeToolbarIndex = -1;
@@ -62,7 +68,6 @@ function registerActiveToolbarIndex() {
 }
 
 function finalizeInit() {
-   console.info("finalizeInit()");
    chrome.browserAction.setBadgeBackgroundColor({color: "mediumpurple"});
    chrome.browserAction.setBadgeText({text: activeToolbarIndex.toString()});
    chrome.browserAction.onClicked.addListener(switch_Empty);
@@ -104,52 +109,16 @@ function moveFolderContents(fromId: string, toId: string, cb: Runnable) {
       if (len === 0) {
          cb();
       } else {
-         move();
+         moveChild(0);
       }
 
-      function move() {
-         var moved = 0;
-         for (let i = 0; i < len; ++i) {
-            const child = children[i];
-            console.groupCollapsed(`SET MOVE CHILD ${i}`);
-            console.info(child);
-            console.groupEnd();
-            chrome.bookmarks.move(child.id, {parentId: toId}, newChild => {
-               moved += 1;
-               console.group(`MOVE CHILD ${i} [${moved}/${len}]`);
-               console.info("FROM", child);
-               console.info("TO", newChild);
-               console.groupEnd();
-               if (moved === len) {
-                  reorder();
-               }
-            });
+      function moveChild(i: number) {
+         if (i === len) {
+            cb();
+            return;
          }
-      }
-
-      function reorder() {
-         var reordered = 0;
-         for (let i = 0; i < len; ++i) {
-            const child = children[i];
-            let index = child.index as any|0;
-            if (index < 0) {
-               console.info("WAS < 0");
-               index = 0;
-            }
-            console.groupCollapsed(`SET RRDR CHILD ${i} to ${index}`);
-            console.info(child);
-            console.groupEnd();
-            chrome.bookmarks.move(child.id, {index}, newChild => {
-               reordered += 1;
-               console.group(`RRDR CHILD ${i} to ${index} [${reordered}/${len}]`);
-               console.info("FROM", child);
-               console.info("TO", newChild);
-               console.groupEnd();
-               if (reordered === len) {
-                  cb();
-               }
-            });
-         }
+         const child = children[i];
+         chrome.bookmarks.move(child.id, {parentId: toId}, () => moveChild(i + 1));
       }
    });
 }
