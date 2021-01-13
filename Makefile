a80f5fea2917943c2e6c48c6d4602203317488ed
coalpha <22552895+coalpha@users.noreply.github.com>
2021-01-12 16:27:35 -0800

    nearly working, I think
    I found a sneaky bugger.

--- Makefile
+++ Makefile
@@ -2,5 +2,10 @@ build:
 	npx tsc
 	node build
 
+release: build
+	npx terser --compress --mangle -- bin/whichmark.js
+
 watch:
 	npx tsc -w
+
+.PHONY: build watch release
--- src/whichmark.ts
+++ src/whichmark.ts
@@ -4,66 +4,124 @@ type Bookmark = B.BookmarkTreeNode;
 type Nullmark = Bookmark | null;
 
 type Consumer<T> = (res: T) => void;
+type Runnable    = ()       => void;
 
-namespace Children {
-   export const getChildByTitle = (haystack: Bookmark[], title: string): Nullmark => {
-      const len = haystack.length;
-      for (var i = 0; i < len; ++i) {
-         const child = haystack[i];
-         if (child.title === title) {
-            return child;
-         }
+const childrenGetChildByTitle = (children: Bookmark[], title: string): Nullmark => {
+   const len = children.length;
+   for (var i = 0; i < len; ++i) {
+      const child = children[i];
+      if (child.title === title) {
+         return child;
       }
-      return null;
    }
+   return null;
 }
 
-namespace Id {
-   export const getChildByTitle = (parentId: string, title: string, cb: Consumer<Nullmark>) =>
-      B.getChildren(parentId, children => cb(Children.getChildByTitle(children, title)))
+const getChildByTitle = (parentId: string, title: string, cb: Consumer<Nullmark>) =>
+B.getChildren(parentId, children => cb(childrenGetChildByTitle(children, title)))
 
-   export const open = (parentId: string, title: string, cb: Consumer<Bookmark>) =>
-      B.getChildren(parentId, children => {
-         const presentChild = Children.getChildByTitle(children, title);
-         if (presentChild) {
-            console.info(`${title} was present`);
-            cb(presentChild);
-            return;
-         }
+const openFolder = (parentId: string, title: string, cb: Consumer<Bookmark>) =>
+   B.getChildren(parentId, children => {
+      const presentChild = childrenGetChildByTitle(children, title);
+      if (presentChild) {
+         console.info(`${title} was present`);
+         cb(presentChild);
+         return;
+      }
 
-         console.info(`${title} is being created`);
-         B.create({title, parentId}, cb);
-      })
-}
+      console.info(`${title} is being created`);
+      B.create({title, parentId}, cb);
+   })
+
 
 const root_id    = "root________";
 const toolbar_id = "toolbar_____";
 const other_id   = "unfiled_____";
 
-const numberOfToolbars = 10;
-const toolbars = Array(numberOfToolbars);
+const numberOfToolbars = 2;
+const benchedToolbarIds: string[] = Array(numberOfToolbars);
+
 console.info("starting");
-Id.open(other_id, "Switchmark", whichmark => {
-   var completed = 0;
-   for (let i = 0; i < numberOfToolbars; ++i) {
-      // very important that i is declared with let
-      setTimeout(() => {
-         Id.open(whichmark.id, i.toString(), toolbar => {
-            console.info(`opened ${i} at index ${toolbar.index}`);
-            toolbars[i] = toolbar;
-            if (++completed === numberOfToolbars) {
-               finalizeInit();
-            }
-         });
-      }, 2000 * i);
-   }
-});
+openFolder(other_id, "Switchmark", registerToolbars);
+
+function registerToolbars(whichmark: Bookmark) {
+   const loop = (i: number): void =>
+      i < numberOfToolbars
+      ? openFolder(whichmark.id, i.toString(), toolbar => {benchedToolbarIds[i] = toolbar.id; loop(i + 1)})
+      : registerActiveToolbarIndex()
+   loop(0);
+}
+
+var activeToolbarIndex = -1;
+
+function registerActiveToolbarIndex() {
+   chrome.storage.sync.get("activeToolbarIndex", obj => {
+      activeToolbarIndex = obj.activeToolbarIndex|0
+      finalizeInit();
+   });
+}
 
 function finalizeInit() {
    console.info("finalizeInit()");
-   chrome.browserAction.onClicked.addListener(switchToolbars);
+   chrome.browserAction.setBadgeBackgroundColor({color: "mediumpurple"});
+   chrome.browserAction.setBadgeText({text: activeToolbarIndex.toString()});
+   chrome.browserAction.onClicked.addListener(switch_Empty);
 }
 
-function switchToolbars() {
-   console.log(toolbars);
+var running = false;
+function switch_Empty() {
+   // we can do something like buffering input in the future
+   if (running) {
+      return;
+   }
+
+   running = true;
+   moveFolderContents(toolbar_id, benchedToolbarIds[activeToolbarIndex], switch_Fill);
+}
+
+function switch_Fill() {
+   activeToolbarIndex += 1;
+   activeToolbarIndex %= numberOfToolbars;
+   moveFolderContents(benchedToolbarIds[activeToolbarIndex], toolbar_id, switch_UpdateBadge);
+}
+
+function switch_UpdateBadge() {
+   chrome.browserAction.setBadgeText({text: activeToolbarIndex.toString()}, switch_UpdateStorage);
+}
+
+function switch_UpdateStorage() {
+   chrome.storage.sync.set({activeToolbarIndex}, switch_Finish);
+}
+
+function switch_Finish() {
+   running = false;
+}
+
+function moveFolderContents(fromId: string, toId: string, cb: Runnable) {
+   B.getChildren(fromId, children => {
+      const len = children.length;
+
+      function move() {
+         var moved = 0;
+         for (let i = 0; i < len; ++i) {
+            chrome.bookmarks.move(children[i].id, {parentId: toId}, () =>
+               ++moved === len
+               && reorder()
+            );
+         }
+      }
+
+      function reorder() {
+         var reordered = 0;
+         for (let i = 0; i < len; ++i) {
+            const {id, index} = children[i];
+            chrome.bookmarks.move(id, {index}, () =>
+               ++reordered === len
+               && cb()
+            );
+         }
+      }
+
+      move();
+   });
 }
